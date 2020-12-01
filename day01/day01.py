293d1dc45d189a6867781e746a766d868b3898c3
Lukas Raffelt <lukas.raffelt@googlemail.com>
2020-12-01 23:30:03 +0100

    Solutions Day1

--- day01/day01.py
+++ day01/day01.py
@@ -1,21 +1,58 @@
 import sys
 sys.path.insert(0, 'D:\\projects\\aoc2020\\')
 
+import inspect
+
+import itertools
+import functools
+
 #from helper import importToIntArray
-from helper import loadingUtils
+from helper import loadingUtils, pretty
 
-
-def inc(x):
-    return x + 1
-
-def run(inFile):
+def runPart1(inFile: str, debug: bool = False) -> int:
+    pretty.sayDayandPart(1,1, inspect.stack()[0].function, inFile)
     result = 0
     numbers = loadingUtils.importToIntArray(inFile)
-    print(numbers)
-    for (a, b) in numbers:
-        print("{} + {} = {}".format(a, b, a+b))
+    results = []
+    for i in range(len(numbers)):
+        a = numbers[i]
+        for j in range(i,len(numbers)):
+            b = numbers[j]
+            if debug: print("{} {} : {} + {} = {}".format(i, j, a, b, a+b))
+            if (a + b == 2020):
+                results.append((a,b))
+    print(results)
+    print(len(results))
+    assert(len(results) != 0)
+    assert(len(results[0]) == 2)
+    result = results[0][0] * results[0][1]
+    print("Numbers {} and {} add to 2020 and have a product of {}".format(results[0][0], results[0][1], result))
+    return result
+
+
+def runPart2(inFile: str, debug: bool = False) -> int:
+    pretty.sayDayandPart(1,2,inspect.stack()[0].function, inFile)
+    result = 0
+    numbers = loadingUtils.importToIntArray(inFile)
+    results = []
+    # Code here 
+    ## itertools.product(numbers, repeat=3) gives many repetitions 
+    for tripel in itertools.combinations(numbers, 3):
+        if debug: print(tripel)
+        if (functools.reduce(lambda a,b : a+b, tripel) == 2020):
+            results.append(tripel)
+    print(results)
+    print(len(results))
+    assert(len(results) != 0)
+    assert(len(results[0]) == 3)
+    winnerTripel = results[0]
+    result = functools.reduce(lambda a,b: a*b, results[0])
+    print("Numbers {} add to 2020 and have a product of {}".format(winnerTripel, result))
     return result
 
 if __name__ == "__main__":
-    run("day01/test1")
+    runPart1("day01/test1", True)
+    runPart1("day01/input")
+    runPart2("day01/test1", True)
+    runPart2("day01/input")
     pass
--- day01/test_day1.py
+++ day01/test_day1.py
@@ -1,9 +1,20 @@
 #from day01.py import *
 import day01
 
-def test_answer():
-    assert day01.inc(4) == 5
 
 def test_part1():
-    result = day01.run("day01/test1")
+    result = day01.runPart1("day01/test1")
     assert result == 514579
+
+def test_part1_real():
+    result = day01.runPart1("day01/input")
+    assert result == 864864
+
+
+def test_part2():
+    result = day01.runPart2("day01/test1")
+    assert result == 241861950
+
+def test_part2_real():
+    result = day01.runPart2("day01/input")
+    assert result == 281473080
