bb71cab7f1ae5ec130d70c890e704c4443319f63
Lukas Raffelt <lukas.raffelt@googlemail.com>
2020-12-02 11:43:45 +0100

    day02

--- day02/day02.py
+++ day02/day02.py
@@ -10,23 +10,78 @@ day = 2
 def getPath():
     return "day{:02d}".format(day)
 
+
+def validate_password_rules(password: str, min_number: int, max_number: int, test_char: str) -> bool:
+    string_array = list(password)
+    number_matching_chars = len(list(filter(lambda char: char == test_char, string_array)))
+    return number_matching_chars >= min_number and number_matching_chars <= max_number
+
+
+def validate_password_rules2(password: str, min_number: int, max_number: int, test_char: str) -> bool:
+    # print(password)
+    string_array = list(password)
+    #print(string_array)
+    val1 = string_array[min_number-1]
+    val2 = string_array[max_number-1]
+    #print("a[{}] = {}    a[{}] = {}".format(min_number, val1, max_number, val2))
+    return (val1 == test_char) ^ (val2 == test_char)
+
+
+def parseInput(password_line: str) -> (str, int, int, str):
+    """
+    1-3 a: abcde
+    """
+    password: str   = ""
+    min_number: int = -1
+    max_number: int = -1
+    test_char: str  = "@"
+    # parse the line
+    (rule, password) = password_line.split(": ")
+    (allowed_range, test_char) = rule.split(" ")
+    (min_number, max_number) = list(map(int, allowed_range.split("-")))
+    # Assert the results
+    assert min_number >= 0
+    assert max_number >= 0
+    assert max_number >= min_number
+    assert len(test_char) == 1
+    assert len(password) >= 0
+    # print("{}-{} times {} required in password {}"
+    #     .format(min_number, max_number, test_char, password))
+    return (password, min_number, max_number, test_char)
+
+
 @Timer()
 def runPart1(inFile: str, debug: bool = False) -> int:
     pretty.printHeader(day, 1, inspect.stack()[0].function, inFile)
     result = 0
-    # code here
+    passwords_list = loadingUtils.importToArray(inFile)
+    results = []
+    for password_line in passwords_list:
+        # print(password_line)
+        (password, min_number, max_number, test_char) = parseInput(password_line)
+        if validate_password_rules(password, min_number, max_number, test_char):
+            results.append(password)
+    result = len(results)
+    print("We have found {} valid passwords.".format(result))
     return result
 
 @Timer()
 def runPart2(inFile: str, debug: bool = False) -> int:
     pretty.printHeader(day, 2, inspect.stack()[0].function, inFile)
     result = 0
-    # code here
+    passwords_list = loadingUtils.importToArray(inFile)
+    results = []
+    for password_line in passwords_list:
+        # print(password_line)
+        (password, min_number, max_number, test_char) = parseInput(password_line)
+        if validate_password_rules2(password, min_number, max_number, test_char):
+            results.append(password)
+    result = len(results)
+    print("We have found {} valid passwords.".format(result))
     return result
 
 if __name__ == "__main__":
     runPart1(getPath() + "/test1", True)
     runPart1(getPath() + "/input1")
-    runPart2(getPath() + "/test2", True)
-    runPart2(getPath() + "/input2")
-
+    runPart2(getPath() + "/test1", True)
+    runPart2(getPath() + "/input1")
--- day02/test_day02.py
+++ day02/test_day02.py
@@ -4,9 +4,19 @@ inputFolder = day.getPath()
 
 def test_part1():
     result = day.runPart1(inputFolder+"/test1")
-    assert result == 0
+    assert result == 2
+
+
+def test_part1_real():
+    result = day.runPart1(inputFolder+"/input1")
+    assert result == 591
 
 
 def test_part2():
-    result = day.runPart2(inputFolder+"/test2")
-    assert result == 0
+    result = day.runPart2(inputFolder+"/test1")
+    assert result == 1
+
+
+def test_part2_real():
+    result = day.runPart2(inputFolder+"/input1")
+    assert result == 335
