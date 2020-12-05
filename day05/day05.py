d0512839610b242e0e40f5b56999e9f57a90f1ef
Lukas Raffelt <lukas.raffelt@googlemail.com>
2020-12-05 13:02:44 +0100

    day05

--- day05/day05.py
+++ day05/day05.py
@@ -8,11 +8,31 @@ DAY = 5
 def get_path():
     return "day{:02d}".format(DAY)
 
+
+def calc_row_column(in_str: str):
+    separator = 7
+    row = in_str[:separator]
+    column = in_str[separator:]
+    row_b = row.replace("F","0")
+    row_b = row_b.replace("B","1")
+    column_b = column.replace("R","1")
+    column_b = column_b.replace("L","0")
+    row_i = int(row_b, 2)
+    col_i = int(column_b, 2)
+    seatID = row_i * 8 + col_i
+    #print("row {} column {} ==> row {} column {} ==> row {} seat {} seatID {}".format(row, column, row_b, column_b, row_i, col_i, seatID))
+    return (row_i, col_i, seatID)
+
 @Timer()
 def run_part_1(in_file: str, debug: bool = False) -> int:
     pretty.printHeader(DAY, 1, inspect.stack()[0].function, in_file)
     result = 0
-    # code here
+    boardingpasses = loadingUtils.importToArray(in_file)
+    seatIDs = []
+    for boardingpass in boardingpasses:
+        (row_i, col_i, seatID) = calc_row_column(boardingpass)
+        seatIDs.append(seatID)
+    result = max(seatIDs)
     print("Result = {}".format(result))
     return result
 
@@ -20,12 +40,29 @@ def run_part_1(in_file: str, debug: bool = False) -> int:
 def run_part_2(in_file: str, debug: bool = False) -> int:
     pretty.printHeader(DAY, 2, inspect.stack()[0].function, in_file)
     result = 0
-    # code here
+    boardingpasses = loadingUtils.importToArray(in_file)
+    seatIDs = []
+    for boardingpass in boardingpasses:
+        (row_i, col_i, seatID) = calc_row_column(boardingpass)
+        seatIDs.append(seatID)
+    seatIDs.sort()
+    print(seatIDs)
+    minSeat = min(seatIDs)
+    maxSeat = max(seatIDs)
+    results = []
+    for seat in range(minSeat, maxSeat):
+        if seat in seatIDs: continue
+        if seat+1 not in seatIDs: continue
+        if seat-1 not in seatIDs: continue
+        print(seat)
+        results.append(seat)
+    assert len(results) == 1
+    result = results[0]
     print("Result = {}".format(result))
     return result
 
 if __name__ == "__main__":
-    run_part_1(get_path() + "/test1", True)
+    #run_part_1(get_path() + "/test1", True)
     run_part_1(get_path() + "/input1")
-    run_part_2(get_path() + "/test2", True)
-    run_part_2(get_path() + "/input2")
+    #run_part_2(get_path() + "/test2", True)
+    run_part_2(get_path() + "/input1")
--- day05/test_day05.py
+++ day05/test_day05.py
@@ -2,21 +2,40 @@ import day05 as day
 
 INPUTFOLDER = day.get_path()
 
-def test_part_1():
-    result = day.run_part_1(INPUTFOLDER+"/test1")
-    assert result == 0
+
+def test_conversion():
+    (row, column, seatID) = day.calc_row_column("FBFBBFFRLR")
+    assert row == 44
+    assert column == 5
+    assert seatID == 357
+
+
+def test_conversion2():
+    (row, column, seatID) = day.calc_row_column("BFFFBBFRRR")
+    assert row == 70
+    assert column == 7
+    assert seatID == 567
+
+
+def test_conversion3():
+    (row, column, seatID) = day.calc_row_column("FFFBBBFRRR")
+    assert row == 14
+    assert column == 7
+    assert seatID == 119
+
+
+def test_conversion4():
+    (row, column, seatID) = day.calc_row_column("BBFFBBFRLL")
+    assert row == 102
+    assert column == 4
+    assert seatID == 820
 
 
 def test_part_1_real():
     result = day.run_part_1(INPUTFOLDER+"/input1")
-    assert result == 0
-
-
-def test_part_2():
-    result = day.run_part_2(INPUTFOLDER+"/test2")
-    assert result == 0
+    assert result == 806
 
 
 def test_part_2_real():
-    result = day.run_part_2(INPUTFOLDER+"/input2")
-    assert result == 0
+    result = day.run_part_2(INPUTFOLDER+"/input1")
+    assert result == 562
