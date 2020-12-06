f8bd57c9e6e1ad253e20a429dc72a4dbb416d14c
Lukas Raffelt <lukas.raffelt@googlemail.com>
2020-12-06 12:31:06 +0100

    day06

--- day06/day06.py
+++ day06/day06.py
@@ -1,5 +1,6 @@
 import sys
 import inspect
+import itertools
 from codetiming import Timer
 sys.path.insert(0, 'D:\\projects\\aoc2020\\')
 from helper import loadingUtils, pretty
@@ -12,7 +13,10 @@ def get_path():
 def run_part_1(in_file: str, debug: bool = False) -> int:
     pretty.printHeader(DAY, 1, inspect.stack()[0].function, in_file)
     result = 0
-    # code here
+    answer_groups = loadingUtils.import_multiline(in_file, separator="empty Line", joinString=False)
+    for answers in answer_groups:
+        answers = set(itertools.chain.from_iterable(map(list, answers)))
+        result += len(answers)
     print("Result = {}".format(result))
     return result
 
@@ -20,12 +24,16 @@ def run_part_1(in_file: str, debug: bool = False) -> int:
 def run_part_2(in_file: str, debug: bool = False) -> int:
     pretty.printHeader(DAY, 2, inspect.stack()[0].function, in_file)
     result = 0
-    # code here
+    answer_groups = loadingUtils.import_multiline(in_file, separator="empty Line", joinString=False)
+    for answers in answer_groups:
+        answers = list(map(set, answers))
+        intersections = set.intersection(*answers)
+        result += len(intersections)
     print("Result = {}".format(result))
     return result
 
 if __name__ == "__main__":
     run_part_1(get_path() + "/test1", True)
     run_part_1(get_path() + "/input1")
-    run_part_2(get_path() + "/test2", True)
-    run_part_2(get_path() + "/input2")
+    run_part_2(get_path() + "/test1", True)
+    run_part_2(get_path() + "/input1")
--- day06/test_day06.py
+++ day06/test_day06.py
@@ -4,19 +4,19 @@ INPUTFOLDER = day.get_path()
 
 def test_part_1():
     result = day.run_part_1(INPUTFOLDER+"/test1")
-    assert result == 0
+    assert result == 11
 
 
 def test_part_1_real():
     result = day.run_part_1(INPUTFOLDER+"/input1")
-    assert result == 0
+    assert result == 6551
 
 
 def test_part_2():
-    result = day.run_part_2(INPUTFOLDER+"/test2")
-    assert result == 0
+    result = day.run_part_2(INPUTFOLDER+"/test1")
+    assert result == 6
 
 
 def test_part_2_real():
-    result = day.run_part_2(INPUTFOLDER+"/input2")
-    assert result == 0
+    result = day.run_part_2(INPUTFOLDER+"/input1")
+    assert result == 3358
