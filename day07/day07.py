8e636216418ac53e6720459520b7f5552af55d20
Lukas Raffelt <lukas.raffelt@googlemail.com>
2020-12-07 06:58:31 +0100

    day07 parser

--- day07/day07.py
+++ day07/day07.py
@@ -8,10 +8,47 @@ DAY = 7
 def get_path():
     return "day{:02d}".format(DAY)
 
+class Color:
+    qualifyer: str = ""
+    name: str = ""
+
+    # def __init__(self, _qual, _name):
+    #     self.qualifyer = _qual
+    #     self.name = _name
+
+    def __init__(self, combined: str):
+        (self.qualifyer, self.name) = combined.split(" ")
+
+    def get_name(self):
+        return self.name
+
+    def get_qualifyer(self):
+        return self.qualifyer
+
+    def display(self):
+        print("I'm a color: {} {}".format(self.qualifyer, self.name))
+
+    def get_color(self):
+        return self.qualifyer+" "+self.name
+
+    def __str__(self):
+        return "{} {}".format(self.qualifyer, self.name)
+
+
+
 @Timer()
 def run_part_1(in_file: str, debug: bool = False) -> int:
     pretty.printHeader(DAY, 1, inspect.stack()[0].function, in_file)
     result = 0
+    rules = loadingUtils.importToArray(in_file)
+    for rule_str in rules:
+        (parent, children) = rule_str[:-1].split(" bags contain ")
+        print("{}  ->  {}".format(parent, children))
+        if children == "no other bags":
+            print("Has no Children")
+            continue
+        for child in children.split(", "):
+            print("  ---- {}".format(child))
     # code here
     print("Result = {}".format(result))
     return result
--- day07/test_day07.py
+++ day07/test_day07.py
@@ -2,9 +2,18 @@ import day07 as day
 
 INPUTFOLDER = day.get_path()
 
+
+def test_color_1():
+    q = "qual"
+    n = "name"
+    a = day.Color(q+" "+n)
+    assert a.get_name() == n
+    assert a.get_qualifyer() == q
+    assert a.get_color() == q+" "+n
+
 def test_part_1():
     result = day.run_part_1(INPUTFOLDER+"/test1")
-    assert result == 0
+    assert result == 4
 
 
 def test_part_1_real():
