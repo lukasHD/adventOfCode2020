7f85a6b004824d2f799991c7066eca1e1e087200
Lukas Raffelt <lukas.raffelt@googlemail.com>
2020-12-09 07:29:35 +0100

    fix linter issues

--- day09/day09.py
+++ day09/day09.py
@@ -1,14 +1,15 @@
 import sys
 import inspect
-from codetiming import Timer
 import queue
 from itertools import combinations
+from codetiming import Timer
 sys.path.insert(0, 'D:\\projects\\aoc2020\\')
 from helper import loadingUtils, pretty
 
 DAY = 9
 def get_path():
     return "day{:02d}".format(DAY)
+
 
 def find_wrong_number(numbers, preamble, debug = False):
     window = queue.Queue()
@@ -17,17 +18,20 @@ def find_wrong_number(numbers, preamble, debug = False):
             print("Fill Queue with {:5}".format(num))
             window.put(num)
             continue
-        allowed_numbers = list(map(lambda pair: pair[0]+pair[1], combinations(list(window.queue), 2)))
-        if debug: print("num {} in allowed? {}".format(num, allowed_numbers))
+        allowed_numbers = list(map(lambda pair: pair[0]+pair[1],
+                                   combinations(list(window.queue), 2)))
+        if debug:
+            print("num {} in allowed? {}".format(num, allowed_numbers))
         if num in allowed_numbers:
             window.get()
             window.put(num)
         else:
             print("{} NOT in the List".format(num))
             return num
+    return 0
 
 
-def find_weaknes(numbers, weakness):
+def find_weaknes(numbers, _weakness):
     for window_length in range(2,50):
         print("~~~~~ window length = {:3}".format(window_length))
         window = queue.Queue()
@@ -36,7 +40,7 @@ def find_weaknes(numbers, weakness):
                 window.put(num)
                 continue
             current_numbers = list(window.queue)
-            if weakness == sum(current_numbers):
+            if _weakness == sum(current_numbers):
                 print("Found weakness with list {}".format(current_numbers))
                 min_n = min(current_numbers)
                 max_n = max(current_numbers)
@@ -44,6 +48,7 @@ def find_weaknes(numbers, weakness):
                 return min_n + max_n
             window.get()
             window.put(num)
+    return 0
 
 
 @Timer()
@@ -56,11 +61,11 @@ def run_part_1(in_file: str, preamble = 25, debug: bool = False) -> int:
     return result
 
 @Timer()
-def run_part_2(in_file: str, weakness = 507622668, debug: bool = False) -> int:
+def run_part_2(in_file: str, _weakness, debug: bool = False) -> int:
     pretty.printHeader(DAY, 2, inspect.stack()[0].function, in_file)
     result = 0
     numbers = loadingUtils.importToIntArray(in_file)
-    result = find_weaknes(numbers, weakness)
+    result = find_weaknes(numbers, _weakness)
     print("Result = {}".format(result))
     return result
 
