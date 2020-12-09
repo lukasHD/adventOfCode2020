cbc7a21f5aff7d1584e8ff99d8fb13bce6059400
Lukas Raffelt <lukas.raffelt@googlemail.com>
2020-12-09 07:23:50 +0100

    day09

--- day09/day09.py
+++ day09/day09.py
@@ -1,6 +1,8 @@
 import sys
 import inspect
 from codetiming import Timer
+import queue
+from itertools import combinations
 sys.path.insert(0, 'D:\\projects\\aoc2020\\')
 from helper import loadingUtils, pretty
 
@@ -8,24 +10,62 @@ DAY = 9
 def get_path():
     return "day{:02d}".format(DAY)
 
+def find_wrong_number(numbers, preamble, debug = False):
+    window = queue.Queue()
+    for k, num in enumerate(numbers):
+        if k < preamble:
+            print("Fill Queue with {:5}".format(num))
+            window.put(num)
+            continue
+        allowed_numbers = list(map(lambda pair: pair[0]+pair[1], combinations(list(window.queue), 2)))
+        if debug: print("num {} in allowed? {}".format(num, allowed_numbers))
+        if num in allowed_numbers:
+            window.get()
+            window.put(num)
+        else:
+            print("{} NOT in the List".format(num))
+            return num
+
+
+def find_weaknes(numbers, weakness):
+    for window_length in range(2,50):
+        print("~~~~~ window length = {:3}".format(window_length))
+        window = queue.Queue()
+        for k, num in enumerate(numbers):
+            if k < window_length:
+                window.put(num)
+                continue
+            current_numbers = list(window.queue)
+            if weakness == sum(current_numbers):
+                print("Found weakness with list {}".format(current_numbers))
+                min_n = min(current_numbers)
+                max_n = max(current_numbers)
+                print("min: {}     max: {}".format(min_n, max_n))
+                return min_n + max_n
+            window.get()
+            window.put(num)
+
+
 @Timer()
-def run_part_1(in_file: str, debug: bool = False) -> int:
+def run_part_1(in_file: str, preamble = 25, debug: bool = False) -> int:
     pretty.printHeader(DAY, 1, inspect.stack()[0].function, in_file)
     result = 0
-    # code here
+    numbers = loadingUtils.importToIntArray(in_file)
+    result = find_wrong_number(numbers, preamble, debug)
     print("Result = {}".format(result))
     return result
 
 @Timer()
-def run_part_2(in_file: str, debug: bool = False) -> int:
+def run_part_2(in_file: str, weakness = 507622668, debug: bool = False) -> int:
     pretty.printHeader(DAY, 2, inspect.stack()[0].function, in_file)
     result = 0
-    # code here
+    numbers = loadingUtils.importToIntArray(in_file)
+    result = find_weaknes(numbers, weakness)
     print("Result = {}".format(result))
     return result
 
 if __name__ == "__main__":
-    run_part_1(get_path() + "/test1", True)
-    # run_part_1(get_path() + "/input1")
-    # run_part_2(get_path() + "/test1", True)
-    # run_part_2(get_path() + "/input1")
+    weakness_test = run_part_1(get_path() + "/test1", 5, True)
+    weakness = run_part_1(get_path() + "/input1")
+    run_part_2(get_path() + "/test1", weakness_test, True)
+    run_part_2(get_path() + "/input1", weakness)
--- day09/test_day09.py
+++ day09/test_day09.py
@@ -3,20 +3,22 @@ import day09 as day
 INPUTFOLDER = day.get_path()
 
 def test_part_1():
-    result = day.run_part_1(INPUTFOLDER+"/test1")
-    assert result == 0
+    result = day.run_part_1(INPUTFOLDER+"/test1", 5)
+    assert result == 127
 
 
 def test_part_1_real():
     result = day.run_part_1(INPUTFOLDER+"/input1")
-    assert result == 0
+    assert result == 507622668
 
 
 def test_part_2():
-    result = day.run_part_2(INPUTFOLDER+"/test2")
-    assert result == 0
+    weak = day.run_part_1(INPUTFOLDER+"/test1", 5)
+    result = day.run_part_2(INPUTFOLDER+"/test1", weak)
+    assert result == 62
 
 
 def test_part_2_real():
-    result = day.run_part_2(INPUTFOLDER+"/input2")
-    assert result == 0
+    weak = day.run_part_1(INPUTFOLDER+"/input1", 25)
+    result = day.run_part_2(INPUTFOLDER+"/input1", weak)
+    assert result == 76688505
