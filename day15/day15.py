32b088933d53e5095199574abec8f1c7dac1bf82
Lukas Raffelt <lukas.raffelt@googlemail.com>
2020-12-15 07:48:02 +0100

    day15

--- day15/day15.py
+++ day15/day15.py
@@ -1,6 +1,8 @@
 import sys
 import inspect
+from collections import defaultdict
 from codetiming import Timer
+from more_itertools import locate
 sys.path.insert(0, 'D:\\projects\\aoc2020\\')
 from helper import loadingUtils, pretty
 
@@ -8,24 +10,57 @@ DAY = 15
 def get_path():
     return "day{:02d}".format(DAY)
 
+
+def get_2020_number(in_list, stop = 2020):
+    # 2020, 2019, or 2021 (0 or 1 as first index?)
+    for i in range(stop-len(in_list)):
+    #for i in range(10):
+        last_number = in_list[-1]
+        index_pos_list = list(locate(in_list, lambda a: a == last_number))
+        if len(index_pos_list) < 2:
+            in_list.append(0)
+        else:
+            in_list.append(index_pos_list[-1] - index_pos_list[-2])
+        #print(in_list)
+    print(in_list[-5:])
+    return in_list[-1]
+
+
+def get_number(in_list, stop = 2020):
+    my_dict = defaultdict(list)
+    for i, n in enumerate(in_list):
+        my_dict[n].append(i)
+    last_number = in_list[-1]
+    for step in range(len(in_list), stop):
+        if len(my_dict[last_number]) < 2:
+            age = 0
+            my_dict[age].append(step)
+        else:
+            age = my_dict[last_number][-1] -my_dict[last_number][-2]
+            my_dict[age].append(step)
+            del my_dict[age][:-2]
+        last_number = age
+    return last_number
+
 @Timer()
-def run_part_1(in_file: str, debug: bool = False) -> int:
+def run_part_1(in_file, debug: bool = False) -> int:
     pretty.printHeader(DAY, 1, inspect.stack()[0].function, in_file)
-    result = 0
+    result = get_2020_number(in_file)
     # code here
     print("Result = {}".format(result))
     return result
 
 @Timer()
-def run_part_2(in_file: str, debug: bool = False) -> int:
+def run_part_2(in_file, debug: bool = False) -> int:
     pretty.printHeader(DAY, 2, inspect.stack()[0].function, in_file)
-    result = 0
+    #result = get_number(in_file, 2020)
+    result = get_number(in_file, 30000000)
     # code here
     print("Result = {}".format(result))
     return result
 
 if __name__ == "__main__":
-    run_part_1(get_path() + "/test1", True)
-    run_part_1(get_path() + "/input1")
-    run_part_2(get_path() + "/test2", True)
-    run_part_2(get_path() + "/input2")
+    run_part_1([0,3,6], True)
+    run_part_1([0,14,1,3,7,9])
+    run_part_2([0,3,6], True)
+    run_part_2([0,14,1,3,7,9])
--- day15/test_day15.py
+++ day15/test_day15.py
@@ -2,21 +2,107 @@ import day15 as day
 
 INPUTFOLDER = day.get_path()
 
+
 def test_part_1():
-    result = day.run_part_1(INPUTFOLDER+"/test1")
-    assert result == 0
+    result = day.run_part_1([0,3,6])
+    assert result == 436
 
 
 def test_part_1_real():
-    result = day.run_part_1(INPUTFOLDER+"/input1")
-    assert result == 0
+    result = day.run_part_1([0,14,1,3,7,9])
+    assert result == 763
 
 
-def test_part_2():
-    result = day.run_part_2(INPUTFOLDER+"/test2")
-    assert result == 0
+def test_01_a():
+    in_list = [0,3,6]
+    result = day.get_number(in_list, 2020)
+    assert result == 436
 
 
-def test_part_2_real():
-    result = day.run_part_2(INPUTFOLDER+"/input2")
-    assert result == 0
+def test_02_a():
+    inp = [1,3,2]
+    result = day.get_number(inp)
+    assert result == 1
+
+
+def test_03_a():
+    inp = [2,1,3]
+    result = day.get_number(inp)
+    assert result == 10
+
+
+def test_04_a():
+    inp = [1,2,3]
+    result = day.get_number(inp)
+    assert result == 27
+
+
+def test_05_a():
+    inp = [2,3,1]
+    result = day.get_number(inp)
+    assert result == 78
+
+
+def test_06_a():
+    inp = [3,2,1]
+    result = day.get_number(inp)
+    assert result == 438
+
+
+def test_07_a():
+    inp = [3,1,2]
+    result = day.get_number(inp)
+    assert result == 1836
+
+
+
+def test_01_b():
+    in_list = [0,3,6]
+    result = day.get_number(in_list, 30000000)
+    assert result == 175594
+
+
+def test_02_b():
+    inp = [1,3,2]
+    result = day.get_number(inp, 30000000)
+    assert result == 2578
+
+
+def test_03_b():
+    inp = [2,1,3]
+    result = day.get_number(inp, 30000000)
+    assert result == 3544142
+
+
+def test_04_b():
+    inp = [1,2,3]
+    result = day.get_number(inp, 30000000)
+    assert result == 261214
+
+
+def test_05_b():
+    inp = [2,3,1]
+    result = day.get_number(inp, 30000000)
+    assert result == 6895259
+
+
+def test_06_b():
+    inp = [3,2,1]
+    result = day.get_number(inp, 30000000)
+    assert result == 18
+
+
+def test_07_b():
+    inp = [3,1,2]
+    result = day.get_number(inp, 30000000)
+    assert result == 362
+
+
+# def test_part_2():
+#     result = day.run_part_2(INPUTFOLDER+"/test2")
+#     assert result == 0
+
+
+# def test_part_2_real():
+#     result = day.run_part_2([0,14,1,3,7,9])
+#     assert result == 1876406
