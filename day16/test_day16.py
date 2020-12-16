adafb29e680ed11ed18a4672f8b909ba7825499b
Lukas Raffelt <lukas.raffelt@googlemail.com>
2020-12-16 10:05:23 +0100

    prepare day 17

--- day16/test_day16.py
+++ day16/test_day16.py
@@ -12,11 +12,11 @@ def test_part_1_real():
     assert result == 24980
 
 
-def test_part_2():
-    result = day.run_part_2(INPUTFOLDER+"/test2")
-    assert result == 0
+# def test_part_2():
+#     result = day.run_part_2(INPUTFOLDER+"/test2")
+#     assert result == 0
 
 
 def test_part_2_real():
-    result = day.run_part_2(INPUTFOLDER+"/input2")
-    assert result == 0
+    result = day.run_part_2(INPUTFOLDER+"/input1")
+    assert result == 809376774329
