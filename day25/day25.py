c69365cceeb3fc17ca8fca1846139c53c7b38a6e
Lukas Raffelt <lukas.raffelt@daimler.com>
2020-12-25 13:07:54 +0100

    day 25 part 1

--- day25/day25.py
+++ day25/day25.py
@@ -8,10 +8,50 @@ DAY = 25
 def get_path():
     return "day{:02d}".format(DAY)
 
+
+def transform(subj_nr: int, loop_size: int) -> int:
+    value = 1
+    for i in range(loop_size):
+        value = value * subj_nr
+        value = value % 20201227
+    return value
+
+
+def crack_loopsize(pub_key: int, subj_nr: int = 7):
+    MAX_TRIES = 50000
+    for i in range(MAX_TRIES):
+        if transform(subj_nr, i) == pub_key:
+            return i
+    raise RuntimeError("Could not crack the card ...")
+
+
+def crack_loopsize_2(pub_key: int, subj_nr: int = 7):
+    MAX_TRIES = 100000000
+    value = 1
+    for i in range(MAX_TRIES):
+        value = value * subj_nr
+        value = value % 20201227
+        if value == pub_key:
+            return i+1
+    raise RuntimeError("Could not crack the card ...")
+
+
 @Timer()
 def run_part_1(in_file: str, debug: bool = False) -> int:
     pretty.printHeader(DAY, 1, inspect.stack()[0].function, in_file)
     result = 0
+    pub_keys = loadingUtils.importToArray(in_file)
+    door_pub = int(pub_keys[0])
+    card_pub = int(pub_keys[1])
+    print("Intercepted Public keys: door = {}; card = {}".format(door_pub, card_pub))
+    door_secret = crack_loopsize_2(door_pub, 7)
+    card_secret = crack_loopsize_2(card_pub, 7)
+    print("Cracked secret loop number: door = {}; card = {}".format(door_secret, card_secret))
+    door_encryption = transform(door_pub, card_secret)
+    card_encryption = transform(card_pub, door_secret)
+    print("Encyption Keys: door = {}; card = {}".format(door_encryption, card_encryption))
+    assert door_encryption == card_encryption
+    result = door_encryption
     # code here
     print("Result = {}".format(result))
     return result
@@ -27,5 +67,5 @@ def run_part_2(in_file: str, debug: bool = False) -> int:
 if __name__ == "__main__":
     run_part_1(get_path() + "/test1", True)
     run_part_1(get_path() + "/input1")
-    run_part_2(get_path() + "/test2", True)
-    run_part_2(get_path() + "/input2")
+    #run_part_2(get_path() + "/test2", True)
+    #run_part_2(get_path() + "/input2")
--- day25/test_day25.py
+++ day25/test_day25.py
@@ -2,14 +2,22 @@ import day25 as day
 
 INPUTFOLDER = day.get_path()
 
+
+def test_transform_1():
+    assert 5764801 == day.transform(7,8)
+
+
+def test_transform_2():
+    assert 17807724 == day.transform(7,11)
+
 def test_part_1():
     result = day.run_part_1(INPUTFOLDER+"/test1")
-    assert result == 0
+    assert result == 14897079
 
 
 def test_part_1_real():
     result = day.run_part_1(INPUTFOLDER+"/input1")
-    assert result == 0
+    assert result == 1478097
 
 
 def test_part_2():
